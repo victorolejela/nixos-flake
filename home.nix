070daad2d853ce20ce6f0194cb35ba48fd1ecd66
Victor Olejela <victorolejela@gmail.com>
2026-03-12 17:07:32 -0400

    Initial NixOS flake configuration

---
+++ home.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "venom";
+  home.homeDirectory = "/home/venom";
+
+  home.stateVersion = "23.11";
+
+  programs.git.enable = true;
+  programs.zsh.enable = true;
+
+  home.packages = with pkgs; [
+    neovim
+    htop
+    tree
+    firefox
+    home-manager
+  ];
+}
